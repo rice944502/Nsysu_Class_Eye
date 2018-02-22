06260ddcade409122fbcd3c4c832f9a4904230a4
Rice <rice944502@gmail.com>
2018-02-22 14:37:40 +0800

    Send status after testing api

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,21 @@ var user = require('../controllers/user');
 router.post('/register', (req, res, next) => {
 	user.register(req.body.email, req.body.password, req.body.confirm, req.body.department)
 		.then((data) => {
-			console.log('0: ' + JSON.stringify(data));
+			res.status(200).json({data});
 		})
 		.catch((data) => {
-			console.log('e: ' + JSON.stringify(data))
+			res.status(400).json({data});
 		})
 })
+
+router.get('/registerVerify', (req, res, next) => {
+	user.registerVerify(req.query.e, req.query.r)
+		.then((data) => {
+			res.status(200).json({data});
+		})
+		.catch((err) => {
+			res.status(400).json({err});
+		})
+});
 
 module.exports = router;
